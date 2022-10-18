8ffceadfd1e4ccb4be67947a758f8dc33325b97a
adrian-albanes <fantabulosoadriangmail.com>
2022-10-18 18:42:05 -0300

    commit2

---
+++ src/main/java/com/probando/principal/Operaciones.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.probando.principal;
+
+/**
+ *
+ * @author Admn
+ */
+public class Operaciones {
+    double resultado;
+    
+    public void Suma(double n1, double n2){
+        resultado = n1+n2;
+    }
+    public void Resta(double n1, double n2){
+        resultado = n1-n2;
+    }
+    public void Multiplicacion(double n1, double n2){
+        resultado = n1*n2;
+    }
+    public void Division(double n1, double n2){
+        resultado = n1/n2;
+    }
+}
